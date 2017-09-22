04b603f6fad1e6a6a3a905d4cd9451c333a8fa07
Alex MacArthur <alex@macarthur.me>
2017-09-21 21:49:58 -0500

    Fix parsing of capital letters and words that end in 'e.'

--- to-ubbi-dubbi.js
+++ to-ubbi-dubbi.js
@@ -1,6 +1,14 @@
 class ToUbbiDubbi {
 
   constructor () {
+    this.vowels = [
+      'a', 
+      'e',
+      'i',
+      'o',
+      'u'
+    ];
+
     this.go();
   }
 
@@ -14,18 +22,28 @@ class ToUbbiDubbi {
           n.nodeType !== Node.TEXT_NODE ||
           !/\S/.test(n.nodeValue)
         ) continue;
-      
-        n.nodeValue = this.translate(n.nodeValue);
+
+      n.nodeValue = n.nodeValue.split(' ').map((word) => {
+        return this.translate(word);
+      });
     }
   }
 
   translate (word) {
-    const vowels = ['a', 'e', 'i', 'o', 'u'];
 
-    let translated = word.split('').map((letter) => {
+    let length = word.length;
 
-      if(vowels.includes(letter)) {
-        return 'ub' + letter;
+    let translated = word.split('').map((letter, index) => {
+
+      let lowerCase = letter.toLowerCase();
+      let ub = letter === lowerCase ? 'ub' : 'UB';
+
+      if(lowerCase === 'e' && (index + 1) === length) {
+        return letter;
+      }
+
+      if(this.vowels.includes(lowerCase)) {
+        return ub + letter;
       }
 
       return letter;
@@ -35,6 +53,6 @@ class ToUbbiDubbi {
   }
 }
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   new ToUbbiDubbi();
 }, false);
